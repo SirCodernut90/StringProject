c74c0d8e0bfd7484b590bd1e26e1d18dabc8b385
Sanjay <sanjaysrinath12@gmail.com>
2023-12-19 10:09:50 -0500

    did pokemon gui that we probably wont need

--- PokemonGUI.java
+++ PokemonGUI.java
@@ -2,7 +2,22 @@ import javax.swing.*;
 
 
 public class PokemonGUI {
+
+    public JFrame frame;
+
     public PokemonGUI() {
-        JFrame frame = new JFrame();
+        frame = new JFrame();
+    }
+
+    public void initialize() {
+        frame.setTitle("Pokemon");
+        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        frame.setSize(500,500);
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
+    public static void main(String[] args) {
+        PokemonGUI p = new PokemonGUI();
+        p.initialize();
     }
 }
