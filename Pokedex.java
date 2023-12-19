4bbc5ba9c5fd00e3befcdbe5d131d3e3affd92d6
zacherypan <zacherypanma@gmail.com>
2023-12-19 14:39:44 -0500

    ui displays pokemon image

--- Pokedex.java
+++ Pokedex.java
@@ -7,36 +7,46 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.*;
+import javax.imageio.*;
 
 public class Pokedex extends JFrame implements ActionListener {
     private JTextField input;
     private JTextArea display;
+    private JLabel imageThumb;
+    private Container container;
 
     // GUI Constructor
     public Pokedex()  {
         super("Pokédex");
-        Container c = getContentPane();
-        c.setLayout(new FlowLayout());
+        container = getContentPane();
+        container.setLayout(new FlowLayout());
+        container.setBackground(Color.WHITE);
 
-        c.add(new JLabel("Pokémon name:"));
-        input = new JTextField(5);
-        input.setBackground(Color.YELLOW);
+        // Input
+        container.add(new JLabel("Pokémon name:"));
+        input = new JTextField(10);
+        input.setBackground(Color.RED);
         input.addActionListener(this);
-        c.add(input);
+        container.add(input);
 
-        display = new JTextArea(10, 50);
+        // Pokémon stats
+        display = new JTextArea(5, 22);
         display.setEditable(false);
         display.setBackground(Color.WHITE);
-        c.add(new JScrollPane(display, ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS,
+        container.add(new JScrollPane(display, ScrollPaneConstants.VERTICAL_SCROLLBAR_NEVER,
                 ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER));
 
+        // Image
+        imageThumb = new JLabel();
+        imageThumb.setPreferredSize(new Dimension(120, 120));
+        container.add(imageThumb);
     }
 
     public void actionPerformed(ActionEvent e)  {
         String pokemonName = "";
         try {
             pokemonName = input.getText();
-            if (pokemonName.equalsIgnoreCase("quit") || pokemonName.equals(" ")) { // Quit program
+            if (pokemonName.equalsIgnoreCase("quit") || pokemonName.equals(" ") || pokemonName.equals("")) { // Quit program
                 return;
             }
             pokemonName = pokemonName.trim().toLowerCase();
@@ -86,14 +96,35 @@ public class Pokedex extends JFrame implements ActionListener {
                 pokemon.setType(type);
                 pokemon.setGen(generation);
             }
-            System.out.println(pokemon);
+
+            // Get image Ex: https://img.pokemondb.net/artwork/walking-wake.jpg
+            try {
+                String urlString = "";
+                if (pokemonName.contains(" ")) { // Check if there is a space in the name, replace with "-" for URL
+                    urlString = "https://img.pokemondb.net/artwork/" + pokemonName.replace(" ", "-").toLowerCase() + ".jpg";
+                } else {
+                    urlString = "https://img.pokemondb.net/artwork/" + pokemonName.toLowerCase() + ".jpg";
+                }
+                URL url = new URI(urlString).toURL();
+                ImageIcon imageIcon = new ImageIcon(url);
+                Image image = imageIcon.getImage();
+                Image rescaled = image.getScaledInstance(120, 120,  java.awt.Image.SCALE_SMOOTH); // Resize image to fit gui
+                imageIcon = new ImageIcon(rescaled); // Convert back to image icon
+                imageThumb.setIcon(imageIcon);
+            } catch (IOException exception) {
+                System.out.println("Image URL not found");
+            }
+
+            display.setText(pokemon.toString());
 
         } catch (MalformedURLException exception) {
+            display.setText("Malformed URL Exception found");
             System.out.println("Malformed URL Exception found");
         } catch (IOException exception) {
+            display.setText("Pokémon not found");
             System.out.println("Pokémon not found");
         } catch (URISyntaxException exception) {
-            System.out.println("Invalid name");
+            display.setText("Invalid name");
         }
     }
 
@@ -104,80 +135,12 @@ public class Pokedex extends JFrame implements ActionListener {
 
         // Create window
         Pokedex window = new Pokedex();
-        window.setBounds(100, 100, 500, 500);
+        window.setBounds(100, 100, 500, 200);
         window.setDefaultCloseOperation(EXIT_ON_CLOSE);
         window.setResizable(false);
         window.setVisible(true);
 
         System.out.println("This program uses data taken from https://pokemondb.net/");
-
-
-//        boolean loop = true;
-//        while (loop) {
-//            try {
-//                System.out.print("\nEnter Pokémon name or \"quit\": "); // Get user input
-//                pokemonName = input.nextLine();
-//                if (pokemonName.equalsIgnoreCase("quit") || pokemonName.equals(" ")) { // Quit program
-//                    loop = false;
-//                    break;
-//                }
-//                pokemonName = pokemonName.trim().toLowerCase();
-//                pokemonName = String.valueOf(pokemonName.charAt(0)).toUpperCase() + pokemonName.substring(1); // Capitalize beginning of each word
-//                if (pokemonName.contains(" ")) { // Has multiple spaces, capitalize
-//                    int firstIndex = pokemonName.indexOf(" ");
-//                    pokemonName = pokemonName.substring(0, firstIndex) + pokemonName.substring(firstIndex, firstIndex + 2).toUpperCase() + pokemonName.substring(firstIndex + 2);
-//                }
-//
-//                String webpageURL = "";
-//                if (pokemonName.contains(" ")) { // Check if there is a space in the name, replace with "-" for URL
-//                    webpageURL = "https://pokemondb.net/pokedex/" + pokemonName.replace(" ", "-").toLowerCase();
-//                } else {
-//                    webpageURL = "https://pokemondb.net/pokedex/" + pokemonName.toLowerCase();
-//                }
-//
-//                URL webpage = new URI(webpageURL).toURL(); // Creates a URL object
-//                URLConnection connection = webpage.openConnection();
-//        		BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-//                String inputLine, line = "";
-//                while ((inputLine = reader.readLine()) != null) {
-//                    line += inputLine;
-//                }
-//                reader.close();
-//
-//                pokemon.setName(pokemonName);
-//
-//                // Check base stats
-//                int position = 0;
-//                position = line.indexOf("cell-num cell-total", position + 1);
-//                if (position > -1) {
-//                    int totalStats = Integer.parseInt(line.substring(position + 21, line.indexOf("</td>", position + 1)));
-//                    pokemon.setTotalStats(totalStats);
-//                }
-//
-//                // Check type(s) and generation
-//                position = 0;
-//                position = line.indexOf("<em>", position + 1);
-//                if (position > -1) {
-//                    String info = line.substring(position, line.indexOf("</abbr>", position + 1)); // Get all information in first sentence
-//                    String type = info.substring(info.indexOf("/type/") + 6, info.indexOf("\" class=\"itype"));
-//                    if (info.indexOf("/type/") != (info.lastIndexOf("/type/"))) { // Check if there is more than one type
-//                        type = type + " + " + info.substring(info.lastIndexOf("/type/") + 6, info.lastIndexOf("\" class=\"itype"));
-//                    }
-//                    //System.out.println(info);
-//                    int generation = Integer.parseInt(info.substring(info.indexOf("Generation") + 11));
-//                    pokemon.setType(type);
-//                    pokemon.setGen(generation);
-//                }
-//                System.out.println(pokemon);
-//
-//            } catch (MalformedURLException e) {
-//                System.out.println("Malformed URL Exception found");
-//            } catch (IOException e) {
-//                System.out.println("Pokémon not found");
-//            } catch (URISyntaxException e) {
-//                System.out.println("Invalid name");
-//            }
-//       }
 
     }
 }
--- PokemonGUI.java
+++ PokemonGUI.java
@@ -1,23 +1,23 @@
-import javax.swing.*;
-
-
-public class PokemonGUI {
-
-    public JFrame frame;
-
-    public PokemonGUI() {
-        frame = new JFrame();
-    }
-
-    public void initialize() {
-        frame.setTitle("Pokemon");
-        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-        frame.setSize(500,500);
-        frame.setLocationRelativeTo(null);
-        frame.setVisible(true);
-    }
-    public static void main(String[] args) {
-        PokemonGUI p = new PokemonGUI();
-        p.initialize();
-    }
-}
+//import javax.swing.*;
+//
+//
+//public class PokemonGUI {
+//
+//    public JFrame frame;
+//
+//    public PokemonGUI() {
+//        frame = new JFrame();
+//    }
+//
+//    public void initialize() {
+//        frame.setTitle("Pokemon");
+//        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+//        frame.setSize(500,500);
+//        frame.setLocationRelativeTo(null);
+//        frame.setVisible(true);
+//    }
+//    public static void main(String[] args) {
+//        PokemonGUI p = new PokemonGUI();
+//        p.initialize();
+//    }
+//}
